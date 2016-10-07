89e80f71b6374e7c8971b38f403daec57ac64972
prezzemolo <git@prezzemolo.ga>
2016-10-08 00:44:51 +0900

    implement bulkbit

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 php-simpleUUID
 ==========
-[![][mit-badge]][mit] [![][issue-badge]][issue]  
-simple UUID version 4 generator written by PHP.
+[![][mit-badge]][mit] [![][php-badge]][php] [![][issue-badge]][issue]  
+simple UUID version 4 generator written by PHP 7.
 
 Usage
 ----------
-See [sample](sample.php).
+* bulkbit  
+See [sample-bulk](sample-bulk.php).
+* each8bit  
+See [sample-each](sample-each.php).
+
+Tips
+----------
+bulkbit up to about three times faster than than each8bit.
+avarage time (gen 1000 UUID):  
+* bulkbit: 0.07s
+* each8bit: 0.2s
 
 LICENSE
 ----------
@@ -15,3 +25,5 @@ The MIT License. See [LICENSE](LICENSE).
 [mit-badge]:https://img.shields.io/badge/license-MIT-444444.svg?style=flat-square
 [issue]: https://github.com/prezzemolo/php-simpleUUID/issues
 [issue-badge]: https://img.shields.io/github/issues/prezzemolo/php-simpleUUID.svg?style=flat-square
+[php]: https://secure.php.net/
+[php-badge]: https://img.shields.io/badge/PHP-7-4F5B93.svg?style=flat-square
--- lib/uuid.php
+++ lib/uuid.php
@@ -6,7 +6,7 @@ namespace simpleUUID;
  */
 class gen {
     /**
-     * separate generate 8bit.
+     * generate separate 8bit.
      */
     public function each8bit () {
         for ($i = 0x1; $i <= 0x20; $i++) {
@@ -15,16 +15,28 @@ class gen {
                     $upperUUID .= '4';
                     break;
                 case $i === 0x11:
-                    $upperUUID .= dechex(0b1000 + mt_rand(0b00, 0b11));
+                    $upperUUID .= dechex(0b1000 + random_int(0b00, 0b11));
                     break;
                 default:
-                    $upperUUID .= dechex(mt_rand(0x0, 0xF));
+                    $upperUUID .= dechex(random_int(0x0, 0xF));
             }
             if ($i === 0x8 || $i === 0xC || $i === 0x10 || $i === 0x14) {
                 $upperUUID .= '-';
             }
         }
-        return mb_strtolower($upperUUID);
+        return strtolower($upperUUID);
+    }
+
+    /**
+     * at once generate as much as possible.
+     */
+    public function bulkbit () {
+        $time_low = bin2hex(random_bytes(4));
+        $time_mid = bin2hex(random_bytes(2));
+        $time_hi_and_version = 0x4000 + (random_int(0x0, 0xFFF));
+        $clock_seq_hi_and_reserved = ((0b1000 + random_int(0b00, 0b11)) * 0x1000) + random_int(0x0, 0xFFF);
+        $clock_seq_low = bin2hex(random_bytes(6));
+        return sprintf('%08s-%04s-%04x-%04x-%012s', $time_low, $time_mid, $time_hi_and_version, $clock_seq_hi_and_reserved, $clock_seq_low);
     }
 }
 ?>
